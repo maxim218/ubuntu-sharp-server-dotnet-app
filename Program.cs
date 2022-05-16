8c89069d8a97d0fef0702ef1d64ae2de43823e26
maxim218 <maxim.kolotovkin@yandex.ru>
2022-05-16 16:17:08 -0400

    Api GET POST + JSON

---
+++ Program.cs
@@ -0,0 +1,64 @@
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+
+app.Map("/api/calculate/summa", appBuilder => {
+    appBuilder.Run(async context => {
+	string a = context.Request.Query["a"];
+	string b = context.Request.Query["b"];
+	int sumInt = int.Parse(a) + int.Parse(b);
+        string resultAnswerString = "Summa: " + sumInt;
+	await context.Response.WriteAsync(resultAnswerString);
+    });
+});
+
+
+app.Map("/api/calculate/multiply/json", appBuilder => {
+    appBuilder.Run(async context => {
+	string a = context.Request.Query["a"];
+	string b = context.Request.Query["b"];
+	int multiplyInt = int.Parse(a) * int.Parse(b);
+	MultiplyJsonRecord obj = new(multiplyInt, "multiply", DateTime.Now.ToString());
+        await context.Response.WriteAsJsonAsync(obj);
+    });
+});
+
+
+app.Map("/api/calculate/post/operation", appBuilder => {
+    appBuilder.Run(async context => {
+	var postBody = await context.Request.ReadFromJsonAsync<OperationJsonRecord>();
+	string operation = postBody.operation;
+	int first = postBody.first;
+	int second = postBody.second;
+	int resultInt = -1;
+	if("div" == operation) resultInt = first / second;
+	if("mod" == operation) resultInt = first % second;
+	OperationAnswer obj = new(resultInt, operation);
+	await context.Response.WriteAsJsonAsync(obj);
+    });
+});
+
+
+app.Run();
+
+
+public record MultiplyJsonRecord(
+    int ansInt, 
+    string messageString, 
+    string dateString
+);
+
+
+public record OperationJsonRecord(
+    string operation,
+    int first,
+    int second
+);
+
+
+public record OperationAnswer(
+    int result,
+    string type
+);
+
+
---
+++ README.md
@@ -0,0 +1,33 @@
+# Server C Sharp Language
+
+### Creating application and run it
+
+```
+dotnet new webapp
+dotnet restore
+dotnet run
+```
+
+### Application programming interface
+
+POST
+
+```
+curl --data '{"operation": "div", "first": 17, "second": 8}' --header "Content-Type: application/json" http://localhost:5288/api/calculate/post/operation
+```
+
+```
+curl --data '{"operation": "mod", "first": 17, "second": 8}' --header "Content-Type: application/json" http://localhost:5288/api/calculate/post/operation
+```
+
+GET
+
+```
+curl "http://localhost:5288/api/calculate/summa?a=12&b=8"
+```
+
+```
+curl "http://localhost:5288/api/calculate/multiply/json?a=5&b=300"
+```
+
+
